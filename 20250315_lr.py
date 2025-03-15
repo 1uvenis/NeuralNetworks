0c91e1511f5bf23bacffdca159936ebe307d8a1b
1uvenis <galiakberov_02@mail.ru>
2025-03-15 11:13:50 +0500

    Графики в зависимости от скорости обучения

--- 20250315_lr.py
+++ 20250315_lr.py
@@ -18,11 +18,12 @@ d_out_value = np.array([1,2,1,1,1],float)
 n_step_value = 0.001
 a = 1
 
-def neural_search():
+def neural_search(n):
     global w11, w12, w21, w22, w1, w2, x_in_value, d_out_value, e
+    e=[]
     i:int
     e_error: float
-    for i in range(9000):
+    for i in range(2000):
         j:int
         for j in range(np.size(d_out_value)):
             x1 = x_in_value[j,0]
@@ -39,22 +40,24 @@ def neural_search():
 
             e_error = ((d_out_value[j] - y) ** 2)/2
 
-            w1 = w1 + n_step_value * e_error*f1
-            w2 = w2 + n_step_value * e_error*f2
-            w11 = w11 + n_step_value * e_error*x1
-            w12 = w12 + n_step_value * e_error*x1
-            w21 = w21 + n_step_value * e_error*x2
-            w22 = w22 + n_step_value * e_error*x2
+            w1 = w1 + n * e_error*f1
+            w2 = w2 + n * e_error*f2
+            w11 = w11 + n * e_error*x1
+            w12 = w12 + n * e_error*x1
+            w21 = w21 + n * e_error*x2
+            w22 = w22 + n * e_error*x2
             j = j + 1
 
         e.append(e_error)
         i = i + 1
 
     plt.plot(range(i), e)
-    plt.show()
 
-neural_search()
 
+neural_search(0.00001)
+neural_search(0.0002)
+neural_search(0.003)
+plt.show()
 print('w1 = ',w1)
 print('w2 = ',w2)
 print('w11 = ',w11)
