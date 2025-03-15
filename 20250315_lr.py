605240b56256d4ec7bef54ad177444672b28794e
1uvenis <galiakberov_02@mail.ru>
2025-03-15 11:58:46 +0500

    Добавлены комментарии

--- 20250315_lr.py
+++ 20250315_lr.py
@@ -2,22 +2,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
+#Ввод начальных значений
 step: int
 e = []
 
+#Весовые коэффициенты 1 слоя
 w11 = 1
 w12 = 1
 w21 = 1
 w22 = 1
 
+#Весовые коэффициенты 2 слоя
 w1 = 0.001
 w2 = 0.001
 
-x_in_value = np.array([[1,1],[1,2],[2,1],[2,2],[1,1]],float)
-d_out_value = np.array([1,2,1,1,1],float)
-n_step_value = 0.001
+x_in_value = np.array([[1,1],[1,2],[2,1],[2,2],[1,1]],float) #Входные параметры
+d_out_value = np.array([1,2,1,1,1],float) #Выходные параметры
+n_step_value = 0.001 #Изначальная скорость обучения
 a = 1
 
+#Функция обучения нейронной сети из 3 нейронов
 def neural_search(n):
     global w11, w12, w21, w22, w1, w2, x_in_value, d_out_value, e
     e=[]
@@ -38,8 +42,9 @@ def neural_search(n):
             s21 = w1*f1 + w2*f2
             y = 1/(1+math.exp(-a*s21))
 
-            e_error = ((d_out_value[j] - y) ** 2)/2
+            e_error = ((d_out_value[j] - y) ** 2)/2 #Нахождение ошибки
 
+            # Изменение весовых коэффициентов
             w1 = w1 + n * e_error*f1
             w2 = w2 + n * e_error*f2
             w11 = w11 + n * e_error*x1
@@ -48,16 +53,17 @@ def neural_search(n):
             w22 = w22 + n * e_error*x2
             j = j + 1
 
-        e.append(e_error)
+        e.append(e_error) #Добавление ошибки в массив
         i = i + 1
 
-    plt.plot(range(i), e)
+    plt.plot(range(i), e) #Добавление массива ошибок в график
 
 
-neural_search(0.00001)
-neural_search(0.0002)
-neural_search(0.003)
-plt.show()
+neural_search(0.00001) #Запуск обучения с шагов 0.00001
+neural_search(0.0002) #Запуск обучения с шагов 0.0002
+neural_search(0.003) #Запуск обучения с шагов 0.003
+plt.show() #Вывод графика
+
 print('w1 = ',w1)
 print('w2 = ',w2)
 print('w11 = ',w11)
